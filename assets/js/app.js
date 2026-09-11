8dca194a04da3aeb56270ceb51a6f5cc6b77c41d
Marcel Marnitz <marcelmarnitz@googlemail.com>
2026-09-11 15:29:06 +0200

    Include app.css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 import { Tooltip } from "bootstrap";
+import "../styles/app.css";
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('[title]').forEach(function(el) {
